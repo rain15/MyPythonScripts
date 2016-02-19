d345962fad0afa510f6afc2020e8d2cfe216bdd4
Hema Pillay <hema.pillay@gmail.com>
2016-02-18 16:21:26 -0800

    updated

--- phone.py
+++ phone.py
@@ -11,3 +11,25 @@ phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 moList = phoneNumRegex.findall(message)
 print(moList)
 
+batRegex = re.compile(r'bat(man|mobile|bat)')
+searchResults = batRegex.search('Batman is the best with a batmobile')
+result = searchResults.group()
+print(result)
+
+batRegex = re.compile(r'Bat(wo)?man')
+searchResults = batRegex.search('The adventures of Batman')
+if searchResults != None:
+    result = searchResults.group()
+    print(result)
+else:
+    print("No results found.")
+
+searchResults = batRegex.search('The adventures of Batwoman')
+if searchResults != None:
+    result = searchResults.group()
+    print(result)
+else:
+    print("No results found.")
+
+
+
