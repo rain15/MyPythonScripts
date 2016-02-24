d5d7d885f28882141bb15331b5c0aa6c50bb9172
Hema Pillay <hema.pillay@gmail.com>
2016-02-24 09:27:25 -0800

    added

---
+++ trafficSimulation.py
@@ -0,0 +1,18 @@
+market_2nd = { 'ns': 'green', 'ew':'red'}
+
+def switchlights(intersection):
+    for key in intersection.keys():
+        if intersection[key] == 'green':
+            intersection[key] = 'yellow';
+        elif intersection[key] == 'yellow':
+            intersection[key] = 'red';
+        elif intersection[key] == 'red':
+            intersection[key] = 'green'
+    assert 'red' in intersection.values(), 'Neither light is red!' + str(intersection)   
+
+print(market_2nd)
+switchlights(market_2nd)
+print(market_2nd)
+
+
+            
