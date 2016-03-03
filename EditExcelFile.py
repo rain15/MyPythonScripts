f3f179b2cc3f3f229ff26f5634653e49c28cc91c
Hema Pillay <hema.pillay@gmail.com>
2016-03-03 10:18:42 -0800

    commited

--- EditExcelFile.py
+++ EditExcelFile.py
@@ -11,3 +11,16 @@ sheet['A2'] = 44
 
 os.chdir('/Users/myfiles')
 wb.save('data1.xlsx')
+
+# create new sheet in workbook
+sheet2 = wb.create_sheet()
+
+print(wb.get_sheet_names())
+
+sheet2.title = 'My New Sheet Name'
+print(wb.get_sheet_names())
+wb.save('data2.xlsx')
+
+wb.create_sheet(index=0, title='My Other Sheet')
+wb.save('data3.xlsx')
+
