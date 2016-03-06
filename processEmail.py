cd853829140e2afed8c194ec6c315d22965cdb28
Hema Pillay <hema.pillay@gmail.com>
2016-03-05 20:32:46 -0800

    committed

---
+++ processEmail.py
@@ -0,0 +1,15 @@
+import smtplib
+
+#conn = smtplib.SMTP('smtp.gmail.com', 587)
+conn = smtplib.SMTP('mail.gmx.com', 25) #465
+
+
+type(conn)
+conn.ehlo()
+conn.starttls()
+conn.login('pythonstudent@gmx.com', 'python2015')
+conn.sendmail('pythonstudent@gmx.com', 'pythonstudent@gmx.com', 'Subject: So long...\n\nDear Al, \nSo long and thanks for all the fish.\n\n-PythonLearner')
+
+conn.sendmail('pythonstudent@gmx.com', 'pythonstudent@gmx.com', 'Subject: So long...\n\nDear Al, \nSo long and thanks for all the chicken.\n\n-PythonLearner')
+
+conn.quit()
